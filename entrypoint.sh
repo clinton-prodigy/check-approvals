4e893c96e296989864a7b9fc49abde3dbed2e3eb
clinton-prodigy <110696738+clinton-prodigy@users.noreply.github.com>
2022-11-04 13:39:04 -0300

    Updated: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,26 +16,14 @@ if [[ -z "$GITHUB_EVENT_PATH" ]]; then
   exit 1
 fi
 
-addLabel=$ADD_LABEL
-if [[ -n "$LABEL_NAME" ]]; then
-  echo "Warning: Plase define the ADD_LABEL variable instead of the deprecated LABEL_NAME."
-  addLabel=$LABEL_NAME
-fi
-
-if [[ -z "$addLabel" ]]; then
-  echo "Set the ADD_LABEL or the LABEL_NAME env variable."
-  exit 1
-fi
-
 URI="https://api.github.com"
 API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 
 action=$(jq --raw-output .action "$GITHUB_EVENT_PATH")
-state=$(jq --raw-output .review.state "$GITHUB_EVENT_PATH")
 number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 
-label_when_approved() {
+check_review_state() {
   # https://developer.github.com/v3/pulls/reviews/#list-reviews-on-a-pull-request
   body=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/pulls/${number}/reviews?per_page=100")
   reviews=$(echo "$body" | jq --raw-output '.[] | {state: .state} | @base64')
@@ -45,39 +33,24 @@ label_when_approved() {
   for r in $reviews; do
     review="$(echo "$r" | base64 -d)"
     rState=$(echo "$review" | jq --raw-output '.state')
-
-    if [[ "$rState" == "APPROVED" ]]; then
+    
+    if [[ "$rState" == "CHANGES_REQUESTED" ]]; then
+      echo '::set-output name=APPROVAL_STATE::CHANGES_REQUESTED'
+      break
+    elif [[ "$rState" == "APPROVED" ]]; then
       approvals=$((approvals+1))
     fi
-
-    echo "${approvals}/${APPROVALS} approvals"
-
-    if [[ "$approvals" -ge "$APPROVALS" ]]; then
-      echo "Labeling pull request"
-
-      curl -sSL \
-        -H "${AUTH_HEADER}" \
-        -H "${API_HEADER}" \
-        -X POST \
-        -H "Content-Type: application/json" \
-        -d "{\"labels\":[\"${addLabel}\"]}" \
-        "${URI}/repos/${GITHUB_REPOSITORY}/issues/${number}/labels"
-
-      if [[ -n "$REMOVE_LABEL" ]]; then
-          curl -sSL \
-            -H "${AUTH_HEADER}" \
-            -H "${API_HEADER}" \
-            -X DELETE \
-            "${URI}/repos/${GITHUB_REPOSITORY}/issues/${number}/labels/${REMOVE_LABEL}"
-      fi
-
+    
+    if [[ approvals -ge ${APPROVALS} ]]; then
+      echo '::set-output name=APPROVAL_STATE::APPROVED'
       break
     fi
+
   done
 }
 
-if [[ "$action" == "submitted" ]] && [[ "$state" == "approved" ]]; then
-  label_when_approved
+if [[ "$action" == "submitted" ]]; then
+  check_review_state
 else
   echo "Ignoring event ${action}/${state}"
 fi
